08d18bfadcfd95c3ce31500408457de8e3231495
dssss01102 <115096906+dssss01102@users.noreply.github.com>
2024-06-20 18:07:52 +0200

    Add files via upload

---
+++ nw/cursor/cursor.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Detect if the user is on a PC
+  const isPC =
+    !/Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+
+  if (isPC) {
+    const cursor = document.getElementById("customCursor");
+
+    document.addEventListener("mousemove", function (e) {
+      cursor.style.left = e.pageX + "px";
+      cursor.style.top = e.pageY + "px";
+    });
+
+    document.querySelectorAll("a").forEach((link) => {
+      link.addEventListener("mouseenter", () => {
+        cursor.classList.add("link-hover");
+      });
+      link.addEventListener("mouseleave", () => {
+        cursor.classList.remove("link-hover");
+      });
+    });
+
+    document.addEventListener("mousedown", () => {
+      // Check if text is being selected
+      document.addEventListener("selectionchange", handleSelectionChange);
+    });
+
+    document.addEventListener("mouseup", () => {
+      // Remove the selectionchange listener and reset the cursor if no text is selected
+      document.removeEventListener("selectionchange", handleSelectionChange);
+      if (document.getSelection().toString() === "") {
+        cursor.classList.remove("text-select");
+      }
+    });
+
+    function handleSelectionChange() {
+      if (document.getSelection().toString() !== "") {
+        cursor.classList.add("text-select");
+      } else {
+        cursor.classList.remove("text-select");
+      }
+    }
+  }
+});
